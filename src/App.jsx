b9aa38154c74af2c6cbb131f328d6ca0499cea43
Yash Kadulkar <yashkadulkar@gmail.com>
2023-09-02 02:44:37 +0530

    Footer added.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Skills from "./components/Skills";
 import Participations from "./components/Participations";
 import Contact from "./components/Contact";
 
+import Footer from "./components/Footer";
+
 import { FaArrowUp } from "react-icons/fa";
 
 
@@ -28,13 +30,13 @@ function App() {
 					<p className="font-bold">Components to be included:</p>
 					<li id="recommendations" className="font-semibold">Testimonials and/or Recommendations (possibly fetched from LinkedIn)</li>
 					<li className="font-semibold">Outro and Salutations</li>
-					<li className="font-semibold">Footer for &copy; copyright and licensing</li>
 					<li className="font-semibold">Sticky Navbar with reduced opacity</li>
 				</ul>
 			</main>
-			<a href="#" id="backtotop" className="p-4 m-4 w-fit fixed bottom-0 right-0 text-xl rounded-full bg-slate-600 text-slate-900 opacity-80 ring-white transition duration-300 hover:opacity-100 hover:ring-2">
+			<a href="#" id="backtotop" className="p-4 m-4 w-fit z-50 fixed bottom-0 right-0 text-xl rounded-full bg-slate-600 text-slate-900 opacity-80 ring-white transition duration-300 hover:opacity-100 hover:ring-2">
 				<FaArrowUp color="white" />
 			</a>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer id="footer" className="py-1 w-full static bottom-0 bg-yellow-600 text-center text-white text-lg">
+        &copy; Copyright and Licensed to <a href="#" className="font-bold transition-all duration-300 hover:text-gray-800">Yash Kadulkar</a>
+    </footer>
+  )
+}
+
+export default Footer
